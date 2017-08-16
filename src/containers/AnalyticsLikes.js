092d1712e1c6c1e49ae042b07d097668530a88ff
noemi <noemiarismendi.cc@gmail.com>
2017-08-16 12:41:17 -0500

    imgGrafica

--- src/containers/AnalyticsLikes.js
+++ src/containers/AnalyticsLikes.js
@@ -4,7 +4,7 @@ export default class AnalyticsLikes extends Component{
     render(){
         return(
            <div>
-            <img src="https://i2.wp.com/images.seroundtable.com/t-summary-impressions-1309440333.png" />
+            <img src="http://www.robertoballester.com/wp-content/uploads/2012/03/Captura-de-pantalla-2012-03-28-a-las-15.13.51-1024x611.png" className="responsive-img" />
            </div>
         )
     }
