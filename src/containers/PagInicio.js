e0ffa524a4d47a341eac1b556873c262981ad05f
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-08-16 14:29:05 -0500

    pagina inicio

--- src/containers/PagInicio.js
+++ src/containers/PagInicio.js
@@ -1,12 +1,45 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-
+import {Row, Col, Icon, Input} from 'react-materialize';
+import logo from '../softtek.png'
+import '../css/NavPrincipal.css';
+import $ from 'jquery';
 
 export default class PagInicio extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        <Link to='/home/ideas'><h1 className='black-text'>Welcome</h1></Link>
+        <Row>
+          <nav className='nav--height'>
+            <Row className="nav-wrapper white grey-text">
+              <Col s={1}>
+                <a href='http://www.softtek.com/' target='_blank' className='brand-logo '><img className='img-logo' src={logo} alt=""/></a>
+              </Col>
+              <Col s={3} offset='s8'>
+                <h5 className='signIn'>Registrarse</h5>
+              </Col>
+            </Row>
+          </nav>
+        </Row>
+        <Row className='contenedor'>
+          <Col m={3} className='form-inicio'>
+            <div>
+              <Row>
+                <h4 className='center'>Inicio de Sesión</h4>
+                <Input id='email' s={12} label=" Correo"  type='email'  validate >
+                  <Icon>mail_outline</Icon>
+                </Input>
+                <Input id='contrasena' s={12} label="Contraseña" validate type='password'>
+                  <Icon>lock_outline</Icon>
+                </Input>
+                <Link to='/home/ideas'>
+                  <a id='submit' type="submit" className='btn btn-signUp-verde'>Iniciar Sesión</a>
+                </Link>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+
       </div>
     )
   }
