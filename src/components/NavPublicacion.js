638227a8dd4553eefa43d87b2bcb70ad7102395c
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-08-16 19:10:54 -0500

    agrega la variable de entorno

--- src/components/NavPublicacion.js
+++ src/components/NavPublicacion.js
@@ -8,9 +8,9 @@ export default class NavPublicacion extends Component {
             <div id="sidebar-publicacion" role="navigation">
                 <div id="sidebar-nav-publicacion">
                     <ul className="nav nav-stacked">
-                        <li className="texto-vertical espacioPorElemento1"><Link to="/home/ideas" className="fontSize">Idea</Link></li>
-                        <li className="texto-vertical espacioPorElemento"><Link to="/home/necesidades" className="fontSize">Necesidad</Link></li>
-                        <li className="texto-vertical espacioPorElemento"><Link to="/home/retos" className="fontSize"> Reto</Link></li>
+                        <li className="texto-vertical espacioPorElemento1"><Link to={public.env.URL + "/home/ideas"} className="fontSize">Idea</Link></li>
+                        <li className="texto-vertical espacioPorElemento"><Link to={public.env.URL + "/home/necesidades"} className="fontSize">Necesidad</Link></li>
+                        <li className="texto-vertical espacioPorElemento"><Link to={public.env.URL + "/home/retos"} className="fontSize"> Reto</Link></li>
                     </ul>
                 </div>
             </div>
