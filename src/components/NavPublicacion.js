b66c9965711d38e181ba3723dfa6d8a93c543124
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-08-16 19:12:04 -0500

    corrige typo

--- src/components/NavPublicacion.js
+++ src/components/NavPublicacion.js
@@ -8,9 +8,9 @@ export default class NavPublicacion extends Component {
             <div id="sidebar-publicacion" role="navigation">
                 <div id="sidebar-nav-publicacion">
                     <ul className="nav nav-stacked">
-                        <li className="texto-vertical espacioPorElemento1"><Link to={public.env.URL + "/home/ideas"} className="fontSize">Idea</Link></li>
-                        <li className="texto-vertical espacioPorElemento"><Link to={public.env.URL + "/home/necesidades"} className="fontSize">Necesidad</Link></li>
-                        <li className="texto-vertical espacioPorElemento"><Link to={public.env.URL + "/home/retos"} className="fontSize"> Reto</Link></li>
+                        <li className="texto-vertical espacioPorElemento1"><Link to={process.env.PUBLIC_URL + "/home/ideas"} className="fontSize">Idea</Link></li>
+                        <li className="texto-vertical espacioPorElemento"><Link to={process.env.PUBLIC_URL + "/home/necesidades"} className="fontSize">Necesidad</Link></li>
+                        <li className="texto-vertical espacioPorElemento"><Link to={process.env.PUBLIC_URL + "/home/retos"} className="fontSize"> Reto</Link></li>
                     </ul>
                 </div>
             </div>
